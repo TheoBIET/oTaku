96d50d208bc4af277289a738f13cd5253d3794dc
Théo BIET <dev.theobiet@gmail.com>
2021-08-05 19:14:25 +0200

    /animes/malID/informations OK

--- api/app/lib/mal-ext/index.js
+++ api/app/lib/mal-ext/index.js
@@ -18,6 +18,23 @@ const mal = {
             console.error(error);
         }
     },
+
+    getDetails: async (id, token) => {
+        try {
+            const results = await axios.get(
+                `${MAL.apiURL}/anime/${id}?${MAL.apiParams}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            console.log(results);
+            return results.data;
+        } catch (error) {
+            console.error(error);
+        }
+    },
 };
 
 module.exports = mal;
