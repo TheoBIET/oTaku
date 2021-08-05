d5038957376637242fc596349d1f5f61d688f467
Théo BIET <dev.theobiet@gmail.com>
2021-08-05 15:24:34 +0200

    sqitch deploy/revert/verify ok

--- migrations/deploy/initialization.sql
+++ migrations/deploy/initialization.sql
@@ -2,6 +2,25 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+CREATE TABLE "role" (
+    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "label" TEXT NOT NULL UNIQUE,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "updated_at" TIMESTAMPTZ
+);
+
+CREATE TABLE "user" (
+    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "username" TEXT NOT NULL UNIQUE,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "my_anime_list_username" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role_id" INT NOT NULL REFERENCES "role" ("id") DEFAULT 1,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "updated_at" TIMESTAMPTZ
+);
 
 COMMIT;
--- migrations/revert/initialization.sql
+++ migrations/revert/initialization.sql
@@ -2,6 +2,6 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+DROP TABLE "user", "role";
 
 COMMIT;
