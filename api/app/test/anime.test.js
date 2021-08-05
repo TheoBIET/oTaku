fc06162a40b991950dc58ba56aab79528919114d
Théo BIET <dev.theobiet@gmail.com>
2021-08-05 14:59:47 +0200

    Itachi fetched from node_modules add to a lib folder cleaning & make JSDoc

---
+++ api/app/test/anime.test.js
@@ -0,0 +1,14 @@
+const itachi = require("../lib");
+
+test('Search "L\'attaque des Titans" on all availables websites', async () => {
+    const results = await itachi.getAnimes("L'attaque des Titans");
+    expect(results.length).toBeGreaterThan(0);
+});
+
+test('Fetch streaming links from "https://vostfree.tv/465-shingeki-kyojin-chuugakkou-vostfr-ddl-streaming-1fichier-uptobox.html"', async () => {
+    const results = await itachi.getStreaming(
+        "vostfree",
+        "https://vostfree.tv/465-shingeki-kyojin-chuugakkou-vostfr-ddl-streaming-1fichier-uptobox.html"
+    );
+    expect(results.length).toBeGreaterThan(0);
+});
