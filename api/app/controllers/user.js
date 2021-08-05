67880183c4f634c56e79beb9d018fee5ef451245
Théo BIET <dev.theobiet@gmail.com>
2021-08-05 17:47:02 +0200

    /user/:username OK | Overloading User.toJSON method for delete password everytimes

--- api/app/controllers/user.js
+++ api/app/controllers/user.js
@@ -1,31 +1,58 @@
 const { User } = require("../models");
 const bcrypt = require("bcrypt");
+const { Op } = require("sequelize");
 
 module.exports = {
-    getProfile(req, res) {
-        res.send("OK");
+    async getProfile(req, res) {
+        const { username } = req.params;
+
+        const user = await User.findOne({
+            where: {
+                username: {
+                    [Op.iLike]: username,
+                },
+            },
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found",
+            });
+        }
+
+        return res.json(user);
     },
     login(req, res) {
-        res.send("OK");
+        res.json("OK");
     },
     async createAccount(req, res) {
         try {
             if (!req.body.password || !req.body.username || !req.body.email) {
-                return res.status(400).send({ message: "Check credentials!" });
+                return res.status(400).json({ message: "Check credentials!" });
             }
 
             const usernameIsTaken = await User.findOne({
-                username: req.body.username,
+                where: {
+                    username: {
+                        [Op.iLike]: req.body.username,
+                    },
+                },
             });
 
             if (usernameIsTaken) {
-                return res.status(400).send({ message: "Username is taken!" });
+                return res.status(400).json({ message: "Username is taken!" });
             }
 
-            const emailIsTaken = await User.findOne({ email: req.body.email });
+            const emailIsTaken = await User.findOne({
+                where: {
+                    email: {
+                        [Op.iLike]: req.body.email,
+                    },
+                },
+            });
 
             if (emailIsTaken) {
-                return res.status(400).send({ message: "Email is taken!" });
+                return res.status(400).json({ message: "Email is taken!" });
             }
 
             const user = new User({
@@ -36,19 +63,22 @@ module.exports = {
 
             await user.save();
 
-            return res.status(200).send({ message: "Account created!", user });
+            return res.status(200).json({ message: "Account created!", user });
         } catch (error) {
             console.error(error);
-            return res.status(500).send({ message: "Something went wrong!" });
+            return res.status(500).json({ message: "Something went wrong!" });
         }
     },
     updateInformations(req, res) {
-        res.send("OK");
+        // TODO: Update the user informations in the database except the password
+        res.json("OK");
     },
     updatePassword(req, res) {
-        res.send("OK");
+        // TODO: Update the user password in the database. Ask for the old password, verify it, then re-insert the new one after checking its strength and hashing it
+        res.json("OK");
     },
     deleteAccount(req, res) {
-        res.send("OK");
+        // TODO: Delete the user account. Request the current password, and change the account status to inactive.
+        // TODO: (Add a database migration, add a column "account_is_deactivated" and "date_of_deactivation" ). After 15 days, if the user has not reconnected, delete the account and the related data. So you have to add a function to set account_is_deactivated back to false in the login method
     },
 };
