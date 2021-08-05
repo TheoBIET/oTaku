6008dd7bc0bdea3fee21cc438b5fb85b9eb38bb1
Théo BIET <dev.theobiet@gmail.com>
2021-08-05 15:16:51 +0200

    Remove Nautijlon search routes. Currently unavailable

--- app/controllers/anime.js
+++ app/controllers/anime.js
@@ -2,43 +2,11 @@ const itachi = require("../lib");
 
 module.exports = {
     search: async (req, res) => {
-        try {
-            const { name } = req.body;
-
-            if (!name) {
-                return res.status(400).send({
-                    message: "You must provide a name.",
-                });
-            }
-
-            const results = await itachi.search(name);
-
-            res.send(results);
-        } catch (error) {
-            res.status(500).send({
-                message: "Internal servor error. Please retry later",
-            });
-        }
+        // TODO : Search with My Anime List API
     },
 
     informations: async (req, res) => {
-        try {
-            const { url } = req.body;
-
-            if (!url) {
-                return res.status(400).send({
-                    message: "You must provide a url.",
-                });
-            }
-
-            const results = await itachi.getInformations(url);
-
-            res.send(results);
-        } catch (error) {
-            res.status(500).send({
-                message: "Internal servor error. Please retry later",
-            });
-        }
+        // TODO : Get informations from MyAnimeList
     },
 
     websites: async (req, res) => {
